625fe75a938d3bf3ab1c80d2020ad1bf0078ae80
wemerson <wemersopm@outlook.com>
2025-05-20 19:25:23 -0300

    Criação do Cod Conta Terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+               
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da Conta");
+        String conta = scanner.next();
+
+                
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu Sobrenome");
+        String sobrenome = scanner.next();
+
+        double saldo = 237.48;
+        
+
+        System.out.println("Ola," + nome + " " + sobrenome + "Obrigado por criar uma conta em nosso banco");
+        System.out.println("Sua agência é " + agencia);
+        System.out.println("Sua agência é " + conta);
+        System.out.println("Seu saldo é " + saldo + " já está disponível");
+
+
+
     }
 }
